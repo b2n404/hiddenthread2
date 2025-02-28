c8d2542f4da6e9909406efe125d7b8be83dbf684
b2n404 <doohick.productions@gmail.com>
2025-02-27 16:49:13 -0800

    lamp script
    lamp turns off and on with button

--- hiddenthreadz217/Assets/scripting/lampcontrol.cs
+++ hiddenthreadz217/Assets/scripting/lampcontrol.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class EnableCompenents : MonoBehaviour
 {
-    public Light lamp;
-    bool lampon;
+    private Light lamp;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
        lamp = GetComponent<Light>();
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            lampon = true;
-            lamp.enabled = false; 
-        }
-    }
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if(other.gameObject.tag == "Player")
+    //     {
+    //         lampon = true;
+    //         lamp.enabled = false; 
+    //     }
+    // }
 
-    void OnTriggerExit (Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            lampon = false;
-            lamp.enabled = true;
-        }
-    }
+    // void OnTriggerExit (Collider other)
+    // {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         lampon = false;
+    //         lamp.enabled = true;
+    //     }
+    // }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.P))
+        {
+            lamp.enabled = !lamp.enabled;
+        }
     }
 
     void LateUpdate()
