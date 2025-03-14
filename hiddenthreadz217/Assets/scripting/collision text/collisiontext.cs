413118d7d16e2c7587cd7a87e774d4d5d7db1dbd
b2n404 <doohick.productions@gmail.com>
2025-03-13 18:41:43 -0700

    Added text to door

--- hiddenthreadz217/Assets/scripting/collision text/collisiontext.cs
+++ hiddenthreadz217/Assets/scripting/collision text/collisiontext.cs
@@ -1,14 +1,17 @@
 using NUnit.Framework.Constraints;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class collisiontext : MonoBehaviour
 {
     //text
     //public GameObject PressX;
     public GameObject bookshelftext;
-    public GameObject walltext;
-
+    //public GameObject walltext;
+    public GameObject exitdoortext;
     public GameObject desktext;
+
+    public GameObject level1doorcollider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -33,14 +36,28 @@ public class collisiontext : MonoBehaviour
             bookshelftext.SetActive(true);
 
         }
-        if(other.gameObject.CompareTag("wall"))
-        {
-            walltext.SetActive(true);
-        }
+        // if(other.gameObject.CompareTag("wall"))
+        // {
+        //     walltext.SetActive(true);
+        // }
 
         if(other.gameObject.CompareTag("desk"))
         {
             desktext.SetActive(true);
+        }
+
+        if(other.gameObject.CompareTag("bedroomdoor1"))
+        {
+            exitdoortext.SetActive(true);
+        }
+
+        if(other.gameObject.CompareTag("key"))
+        {
+            Destroy(level1doorcollider);
+        }
+        if(other.gameObject.CompareTag("TOHALLWAY"))
+        {
+            SceneManager.LoadScene("Hallway");
         }
     }
 
@@ -50,8 +67,9 @@ public class collisiontext : MonoBehaviour
             {
             //PressX.SetActive(false);
             bookshelftext.SetActive(false);
-            walltext.SetActive(false);
+            //walltext.SetActive(false);
             desktext.SetActive(false);
+            exitdoortext.SetActive(false);
             }
         
     }
