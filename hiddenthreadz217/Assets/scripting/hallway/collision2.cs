da03f3084c1033445d6a306807e5dd5e3529f677
b2n404 <doohick.productions@gmail.com>
2025-03-31 19:09:24 -0700

    added transition text + kyrene's stools

--- hiddenthreadz217/Assets/scripting/hallway/collision2.cs
+++ hiddenthreadz217/Assets/scripting/hallway/collision2.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class collision2 : MonoBehaviour
 {
+    public string targetTag = "Player";
+    private bool inTriggerArea = false;
+    public GameObject question;
+    //public GameObject yes;
+    //public GameObject no;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,14 +17,38 @@ public class collision2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(inTriggerArea && Input.GetKey(KeyCode.X))
+        {
+            SceneManager.LoadScene("Main2-kitchen-livingroom");
+        }
+
+        if(inTriggerArea && Input.GetKey(KeyCode.O))
+        {
+            question.SetActive(false);
+            //yes.SetActive(false);
+            //no.SetActive(false);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("tokitchen"))
+        inTriggerArea = true;
+        if(other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Main2-kitchen-livingroom");
+            question.SetActive(true);
+            //yes.SetActive(true);
+            //no.SetActive(false);
+
         }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTriggerArea = false;
+        question.SetActive(false);
+        //yes.SetActive(false);
+        //no.SetActive(true);
+
     }
 }
